9760708063d33417acffeee16f5be4644e89cf64
Chema Martin Ramos <josemmr2009@hotmail.com>
2019-12-19 15:33:42 +0100

    1.0

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.net.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -7,136 +9,134 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
-class ServerClass implements Runnable {
-    private int PORT; // server port
 
-    public ServerClass(int PORT) {
-        this.PORT = PORT;
+public class Node {
+
+
+    public static ArrayList<Reading> medias = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        assert (args.length == 2);
+
+
+        int[] ports = IntStream.rangeClosed(1000, 1050).toArray();
+        int serverPort = Integer.parseInt(args[0]);
+        //int serverPort = Integer.parseInt(new Scanner(System.in).next());
+        new Thread(new ServerClass(serverPort)).start();
+        int NodesNumber = Integer.parseInt(args[1]);
+        //int NodesNumber = Integer.parseInt(new Scanner(System.in).next());
+
+        //cada 5seg?
+        EmulatedSystemClock emulatedSystemClock = new EmulatedSystemClock();
+        new Scanner(System.in).next();
+        long start_t = System.currentTimeMillis();
+
+        System.out.println((System.currentTimeMillis() - start_t) / 1000);
+        while ((System.currentTimeMillis() - start_t) / 1000f < (5.00f - 5.00f * 0.2)) {//MAL
+            long tReading = emulatedSystemClock.currentTimeMillis();
+            for (int a = 0; a < NodesNumber; a++) {
+                if (ports[a] != serverPort) {
+                    new Thread(new ClientClass(ports[a], tReading)).start();//tiempo de medida
+                }
+            }
+            Thread.sleep(1000);
+
+        }
+        Thread.sleep(10000);
+        //
+        if (!medias.isEmpty()) {
+            System.out.print("Temp: ");
+            if (medias.stream().filter(reading -> reading.getTemp() != -1f).mapToDouble(Reading::getTemp).filter(medida -> medida != -1f).average().isPresent())
+                System.out.println(medias.stream().filter(reading -> reading.getTemp() != -1f).mapToDouble(Reading::getTemp).filter(medida -> medida != -1f).average().getAsDouble());
+            System.out.print("Press: ");
+            if (medias.stream().filter(reading -> reading.getPress() != -1f).mapToDouble(Reading::getPress).filter(medida -> medida != -1f).average().isPresent())
+                System.out.println(medias.stream().filter(reading -> reading.getPress() != -1f).mapToDouble(Reading::getPress).filter(medida -> medida != -1f).average().getAsDouble());
+            System.out.print("Hum: ");
+            if (medias.stream().filter(reading -> reading.getHum() != -1f).mapToDouble(Reading::getHum).filter(medida -> medida != -1f).average().isPresent())
+                System.out.println(medias.stream().filter(reading -> reading.getHum() != -1f).mapToDouble(Reading::getHum).filter(medida -> medida != -1f).average().getAsDouble());
+            System.out.print("Co: ");
+            if (medias.stream().filter(reading -> reading.getCo() != -1f).mapToDouble(Reading::getCo).filter(medida -> medida != -1f).average().isPresent())
+                System.out.println(medias.stream().filter(reading -> reading.getCo() != -1f).mapToDouble(Reading::getCo).filter(medida -> medida != -1f).average().getAsDouble());
+            System.out.print("NO2: ");
+            if (medias.stream().filter(reading -> reading.getNo2() != -1f).mapToDouble(Reading::getNo2).filter(medida -> medida != -1f).average().isPresent())
+                System.out.println(medias.stream().filter(reading -> reading.getNo2() != -1f).mapToDouble(Reading::getNo2).filter(medida -> medida != -1f).average().getAsDouble());
+            System.out.print("SO2: ");
+            if (medias.stream().filter(reading -> reading.getSo2() != -1f).mapToDouble(Reading::getSo2).filter(medida -> medida != -1f).average().isPresent())
+                System.out.println(medias.stream().filter(reading -> reading.getSo2() != -1f).mapToDouble(Reading::getSo2).filter(medida -> medida != -1f).average().getAsDouble());
+
+        }
+        //System.out.println(medias);
+
+
     }
 
-    public void run() {
-        long startTime = System.nanoTime();
-        byte[] rcvBuf = new byte[256]; // Buffer for rcv bytes received bytes
-        byte[] sendBuf = new byte[256];// sent bytes
-        String rcvStr;
+    private static class ClientClass implements Runnable {
+        private int portSend;
+        private long tReading;
 
-        // create a UDP socket and bind it to the specified port on the local
-        // host
-        DatagramSocket socket = null; //QUE lost-rate??¿?
-        try {
-            socket = new SimpleSimulatedDatagramSocket(PORT, 0.1, 1000);
-        } catch (SocketException e) {
-            e.printStackTrace();
+        public ClientClass(int portSend, long tReading) {
+            this.portSend = portSend;
+            this.tReading = tReading;
         }
-        StringBuffer rcvString = new StringBuffer();
-        while (true) {
-            // create a DatagramPacket for receiving packets
-            DatagramPacket packet = new DatagramPacket(rcvBuf, rcvBuf.length);
 
-            // receive packet
+        public void run() {
+
+            //long startTime = System.nanoTime();//usar emulated system clock¿?
+
+
+            //each second
+            Reading temp = new Reading();
+            String sendString = Arrays.toString(temp.generateReadings(tReading));
+
+
+            byte[] rcvBuf = new byte[256]; // received bytes
+
+            // encode this String into a sequence of bytes using the platform's
+            // default charset and store it into a new byte array
+
+            // determine the IP address of a host, given the host's name
+            InetAddress address = null;
             try {
-                socket.receive(packet); //RECVFROM
-            } catch (IOException e) {
+                address = InetAddress.getByName("localhost");
+            } catch (UnknownHostException e) {
                 e.printStackTrace();
             }
 
-            // construct a new String by decoding the specified subarray of
-            // bytes
-            // using the platform's default charset
-            String tempRcv = new String(packet.getData(), packet.getOffset(), packet.getLength());
-            rcvString.append(tempRcv);
-            if(tempRcv.equals("]")){
-        System.out.println("Server RCV: "+rcvString);
-        rcvString = new StringBuffer();
-
-            }
-
-
-
-            // encode a String into a sequence of bytes using the platform's
-            // default charset
-
-
-            // create a DatagramPacket for sending packets
-            DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length, packet.getAddress(), packet.getPort());
-
-            // send packet
+            // create a datagram socket and bind it to any available
+            // port on the local host
+            //DatagramSocket socket = new SimulatedDatagramSocket(0.2, 1, 200, 50); //SOCKET
+            DatagramSocket socket = null; //SOCKET
             try {
-                socket.send(sendPacket); //SENDTO
-            } catch (IOException e) {
+                socket = new SimpleSimulatedDatagramSocket(0.4, 1000);
+            } catch (SocketException e) {
                 e.printStackTrace();
             }
+            System.out.print("Client sends: ");
 
-        }
-    }
-
-    //------------------------------------------------------------------------------------------------------------------
-}
-
-class ClientClass implements Runnable {
-    private int portSend;
-
-    public ClientClass(int portSend) {
-        this.portSend = portSend;
-    }
-
-    public void run() {
-        //long startTime = System.nanoTime();//usar emulated system clock¿?
-        EmulatedSystemClock eck = new EmulatedSystemClock();
-        long startTime = eck.currentTimeMillis();
-
-
-        //each second
-        Reading temp = new Reading();
-        String sendString = Arrays.toString(temp.generateReadings(eck.currentTimeMillis() - startTime));
-
-        byte[] rcvBuf = new byte[256]; // received bytes
-
-        // encode this String into a sequence of bytes using the platform's
-        // default charset and store it into a new byte array
-
-        // determine the IP address of a host, given the host's name
-        InetAddress address = null;
-        try {
-            address = InetAddress.getByName("localhost");
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
-        // create a datagram socket and bind it to any available
-        // port on the local host
-        //DatagramSocket socket = new SimulatedDatagramSocket(0.2, 1, 200, 50); //SOCKET
-        DatagramSocket socket = null; //SOCKET
-        try {
-            socket = new SimpleSimulatedDatagramSocket(0.4, 1000);
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-        System.out.print("Client sends: ");
-
-        StringBuffer receiveString = packageManagement(sendString, rcvBuf, portSend, address, socket);
-        System.out.println(receiveString);
-        if (receiveString.length() == 0 || receiveString.length() != sendString.length()) {
-            StringBuffer stringBuffer2 = packageManagement(sendString, rcvBuf, portSend, address, socket);
-            System.out.println("ACK1 fail");
-            if (stringBuffer2.length() == 0 || stringBuffer2.length() != sendString.length()) {
-                socket.close();
-                System.out.println("ACK2 fail");
-                return;//???
+            String receiveString = packageManagement(sendString, rcvBuf, portSend, address, socket);
+            System.out.println(receiveString);
+            if (receiveString.length() == 0 || Integer.parseInt(receiveString) != sendString.length()) {
+                String stringBuffer2 = packageManagement(sendString, rcvBuf, portSend, address, socket);
+                System.out.println("ACK1 fail");
+                if (stringBuffer2.length() == 0 || stringBuffer2.length() != sendString.length()) {
+                    socket.close();
+                    System.out.println("ACK2 fail");
+                    return;//???
+                }
             }
+            System.out.println("Client received: " + receiveString);
+            if (!medias.contains(temp))
+                //if (medias.stream().noneMatch(reading -> reading.getNo2().equals(temp.getNo2())))
+                medias.add(temp);
+            // close the datagram socket
+            socket.close(); //CLOSE
         }
-        System.out.println("Client received: " + receiveString);
 
-        // close the datagram socket
-        socket.close(); //CLOSE
-    }
+        private String packageManagement(String sendString, byte[] rcvBuf, int port, InetAddress address, DatagramSocket socket) {
+            // send each character as a separate datagram packet
 
-    private StringBuffer packageManagement(String sendString, byte[] rcvBuf, int port, InetAddress address, DatagramSocket socket) {
-        // send each character as a separate datagram packet
 
-        for (int i = 0; i < sendString.length(); i++) {
-            byte[] sendBuf = new byte[1];// sent bytes
-            sendBuf[0] = (byte) sendString.charAt(i);
+            byte[] sendBuf = sendString.getBytes(StandardCharsets.UTF_8);
 
             // create a datagram packet for sending data
             DatagramPacket packet = new DatagramPacket(sendBuf, sendBuf.length,
@@ -148,68 +148,104 @@ class ClientClass implements Runnable {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.print(new String(sendBuf));
-        }
-        System.out.println("");
+            System.out.println(new String(sendBuf));
 
-        //==========================================================================================================
-        StringBuffer receiveString = new StringBuffer();//Buffer used for received String
+            String receiveString = "";//Buffer used for received String
 
-        while (true) {
-            // create a datagram packet for receiving data
-            DatagramPacket rcvPacket = new DatagramPacket(rcvBuf, rcvBuf.length);
+            while (true) {
+                // create a datagram packet for receiving data
+                DatagramPacket rcvPacket = new DatagramPacket(rcvBuf, rcvBuf.length);
 
-            try {
-                // receive a datagram packet from this socket
-                socket.receive(rcvPacket); //RECVFROM
+                try {
+                    // receive a datagram packet from this socket
+                    socket.receive(rcvPacket); //RECVFROM
 
 
-            } catch (SocketTimeoutException e) {
-                break;
-            } catch (IOException ex) {
-                Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            // construct a new String by decoding the specified subarray of bytes
-            // using the platform's default charset
-            receiveString.append(new String(rcvPacket.getData(), rcvPacket.getOffset(), rcvPacket.getLength()));
-
-        }
-        return receiveString;
-    }
-
-}
-
-public class Node {
-
-
-    public static void main(String[] args) throws InterruptedException {
-        assert (args.length==2);
-
-        ArrayList<Thread> hilosSend = new ArrayList<>();
-        int[] ports = IntStream.rangeClosed(1000, 1050).toArray();
-        int serverPort= Integer.parseInt(args[0]);
-        //int serverPort = Integer.parseInt(new Scanner(System.in).next());
-        new Thread(new ServerClass(serverPort)).start();
-        int NodesNumber= Integer.parseInt(args[1]);
-        //int NodesNumber = Integer.parseInt(new Scanner(System.in).next());
-
-        //cada 5seg?
-        EmulatedSystemClock emulatedSystemClock = new EmulatedSystemClock();
-        new Scanner(System.in).next();
-        while (true) {
-            while (emulatedSystemClock.currentTimeMillis() > (5.00f - 5.00f * 0.2)) {
-                Thread tempThread;
-                for (int a = 0; a < NodesNumber; a++) {
-                    if (ports[a] != serverPort) {
-                        tempThread = new Thread(new ClientClass(ports[a]));//tiempo de medida
-                        hilosSend.add(tempThread);
-                        tempThread.start();
-                    }
+                } catch (SocketTimeoutException e) {
+                    break;
+                } catch (IOException ex) {
+                    Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                Thread.sleep(1000);
+
+                // construct a new String by decoding the specified subarray of bytes
+                // using the platform's default charset
+                receiveString = (new String(rcvPacket.getData(), rcvPacket.getOffset(), rcvPacket.getLength()));
+
             }
-            System.out.println("medias?");
+            return receiveString;
         }
+
     }
+
+
+    private static class ServerClass implements Runnable {
+        private int PORT; // server port
+
+        public ServerClass(int PORT) {
+            this.PORT = PORT;
+        }
+
+        public void run() {
+            long startTime = System.nanoTime();
+            byte[] rcvBuf = new byte[256]; // Buffer for rcv bytes received bytes
+            byte[] sendBuf;// sent bytes
+            String rcvStr;
+
+            // create a UDP socket and bind it to the specified port on the local
+            // host
+            DatagramSocket socket = null; //QUE lost-rate??¿?
+            try {
+                socket = new SimpleSimulatedDatagramSocket(PORT, 0.4, 1000);
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+            StringBuffer rcvString = new StringBuffer();
+            while (true) {
+                // create a DatagramPacket for receiving packets
+                DatagramPacket packet = new DatagramPacket(rcvBuf, rcvBuf.length);
+
+                // receive packet
+                try {
+                    socket.receive(packet); //RECVFROM
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // construct a new String by decoding the specified subarray of
+                // bytes
+                // using the platform's default charset
+                String tempRcv = new String(packet.getData(), packet.getOffset(), packet.getLength());
+                System.out.println(tempRcv);
+
+                String[] splitString = tempRcv.substring(1, tempRcv.length() - 1).split(",");
+                int a = 0;
+                Reading temp = new Reading(Float.parseFloat(splitString[a++]), Float.parseFloat(splitString[a++]), Float.parseFloat(splitString[a++]),
+                        Float.parseFloat(splitString[a++]), Float.parseFloat(splitString[a++]), Float.parseFloat(splitString[a++]), Float.parseFloat(splitString[a].substring(1)));
+
+
+                if (!medias.contains(temp))
+                    medias.add(temp);
+                System.out.println(tempRcv);
+                sendBuf = String.valueOf(tempRcv.length()).getBytes();
+
+                // encode a String into a sequence of bytes using the platform's
+                // default charset
+
+
+                // create a DatagramPacket for sending packets
+                DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length, packet.getAddress(), packet.getPort());
+
+                // send packet
+                try {
+                    socket.send(sendPacket); //SENDTO
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+        //------------------------------------------------------------------------------------------------------------------
+    }
+
 }
--- src/Reading.java
+++ src/Reading.java
@@ -3,8 +3,19 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Reading {
+    public Reading(Float temp, Float press, Float hum, Float co, Float no2, Float so2, Float time) {
+        this.temp = temp;
+        this.press = press;
+        this.hum = hum;
+        this.co = co;
+        this.no2 = no2;
+        this.so2 = so2;
+        this.time = time;
+    }
+
     //Temperature,Pressure,Humidity,CO,NO2,SO2,
     private Float temp;//0
     private Float press;
@@ -12,14 +23,16 @@ public class Reading {
     private Float co;
     private Float no2;
     private Float so2;
+    private Float time;
     //vector and scalar timestamp¿? for sorting
-    public Reading(Float temp, Float press, Float hum, Float co, Float no2, Float so2) {
-        this.temp = temp;
-        this.press = press;
-        this.hum = hum;
-        this.co = co;
-        this.no2 = no2;
-        this.so2 = so2;
+
+
+    public Float getTime() {
+        return time;
+    }
+
+    public void setTime(Float time) {
+        this.time = time;
     }
 
     public Reading() {
@@ -73,35 +86,51 @@ public class Reading {
         this.so2 = so2;
     }
 
-    public float[] generateReadings(long currentTime){
+    public float[] generateReadings(long currentTime) {
         //currentTime = System.nanoTime() - start
         //start = int star_t = (int) System.nanoTime(); At beggining of life of node
 
-            String[] t_val = new String[6];
-            try {
-                t_val = Files.readAllLines(Paths.get("src/readings.csv")).get((int) Math.abs(currentTime % 101 + 1)).split(",");//Copiado en proyecto bien?¡¿
-            } catch (InvalidPathException | IOException e) {
-                System.out.println("Error path");
-            }
-            int sizeT_val = t_val.length;
-            t_val = Arrays.copyOf(t_val, 6);
-            Arrays.fill(t_val, sizeT_val, 6, "");
-            for (int x = 0; x <= 5; x++) {
-                if (t_val[x].isEmpty())
-                    t_val[x] = String.valueOf(-1);
-            }
-            setTemp(Float.parseFloat(t_val[0]));
-            setPress(Float.parseFloat(t_val[1]));
-            setHum(Float.parseFloat(t_val[2]));
-            setCo(Float.parseFloat(t_val[3]));
-            setNo2(Float.parseFloat(t_val[4]));
-            setSo2(Float.parseFloat(t_val[5]));
+        String[] t_val = new String[6];
+        try {
+            t_val = Files.readAllLines(Paths.get("src/readings.csv")).get((int) Math.abs(currentTime % 101)).split(",");//Copiado en proyecto bien?¡¿
+        } catch (InvalidPathException | IOException e) {
+            System.out.println("Error path");
+        }
+        int sizeT_val = t_val.length;
+        t_val = Arrays.copyOf(t_val, 6);
+        Arrays.fill(t_val, sizeT_val, 6, "");
+        for (int x = 0; x <= 5; x++) {
+            if (t_val[x].isEmpty())
+                t_val[x] = String.valueOf(-1);
+        }
+        setTemp(Float.parseFloat(t_val[0]));
+        setPress(Float.parseFloat(t_val[1]));
+        setHum(Float.parseFloat(t_val[2]));
+        setCo(Float.parseFloat(t_val[3]));
+        setNo2(Float.parseFloat(t_val[4]));
+        setSo2(Float.parseFloat(t_val[5]));
+        setTime((float)currentTime);
+
+        return new float[]{getTemp(), getPress(), getHum(), getCo(), getNo2(), getSo2(),getTime()};
 
 
-             float[] exit = {getTemp(),getPress(),getHum(),getCo(),getNo2(),getSo2()};
-             return exit;
+    }
 
 
+    public boolean equals(Reading o) {
 
+        if (o == null || getClass() != o.getClass()) return false;
+        return time.equals(o.time) &&
+                temp.equals(o.temp) &&
+                press.equals(o.press) &&
+                hum.equals(o.hum) &&
+                co.equals(o.co) &&
+                no2.equals(o.no2) &&
+                so2.equals(o.so2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(temp, press, hum, co, no2, so2, time);
     }
 }
